c3cb419db2571c0aa1d9757f3ab9635713983dca
Jurriaan <jurriaan@iksi.cc>
2017-02-14 12:24:50 +0100

    fullTitle site method

---
+++ site/plugins/site-methods.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * returns a Field object with the fullTitle for the in title bar
+ */
+site::$methods['fullTitle'] = function($site) {
+  $page = $site->page();
+  $pageTitle = $page->title()->value();
+  $siteTitle = $site->title()->value();
+
+  if ($page->isHomepage()) {
+    $fullTitle = $siteTitle;
+  } else {
+    $fullTitle = "$pageTitle — $siteTitle";
+  }
+
+  return new Field($site, 'fullTitle', $fullTitle);
+};
